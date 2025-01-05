b6a8228db6e8d5c197ee367c4e85f477d28f2b79
SanishPoudel <mr.sanish.poudel@gmail.com>
2025-01-05 22:27:23 +1100

    wrote vague pseudo code to plan out everything

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import "./style.css"
 console.log("hello");
+
+// how to start
+// first i'll need an empty list to hold the projects
+// when a new project is added it'll be appended to this list
+
+// the projects themselves will be a dictionary 
+// the key will be the name of the project and the value will be the list of tasks
+// the tasks will be a custom data type that will have various features
+
+// so a function to create "tasks" which will be a custom object this can be a factory function or a class
+// another function to add these tasks to their projects this can just be projects["tasks"] with both of those as parameters
+// and another function to add the projects to the list. this may be redundant or not needed later on.
+// another function to link this with the dom
