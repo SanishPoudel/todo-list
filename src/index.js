a94d7e06f2559ef75bd0c52f3cf0034e1c9cc1c8
SanishPoudel <mr.sanish.poudel@gmail.com>
2025-01-06 20:22:12 +1100

    added classes and function based on pseudo code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import "./style.css"
-console.log("hello");
+// import "./style.css"
+
 
 // how to start
 // first i'll need an empty list to hold the projects
@@ -13,3 +13,40 @@ console.log("hello");
 // another function to add these tasks to their projects this can just be projects["tasks"] with both of those as parameters
 // and another function to add the projects to the list. this may be redundant or not needed later on.
 // another function to link this with the dom
+
+
+const projects = []; // this will be the projects list
+
+class Task{
+    constructor(name, status, dueDate) {
+        this.name = name;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+}
+
+function addProject(projectName, taskName) {
+    // this should check if the projectName already exists if it does then just add to its list otherwise create a new one.
+    let dict = {};
+    console.log("before ", projects)
+    if (projects.includes(dict[projectName])) {
+        dict[projectName].push(taskName);
+        projects.push(dict);
+        console.log("duplicate case " + projects)
+    }
+    else {
+        dict[projectName] = [taskName];
+        projects.push(dict);
+        console.log("original ", projects)
+    }
+    
+     
+}
+
+let value = new Task("name", true, "tomorrow");
+newValue = new Task("newName", false, "today");
+addProject("key", value);
+addProject("key", newValue);
+
+
+// console.log(projects[0]["key"]);
